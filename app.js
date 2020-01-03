38876e7f02247b4a92ad421e8db77ccb6ba40d61
gamesketches <sam.von.ehren@gmail.com>
2020-01-03 01:02:39 -0500

    Got a whole little game interaction going

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
 const { App } = require('@slack/bolt');
 
+function MakeButtonCard(text) {
+	returnVal = {
+			"type":"section",
+			"text": {
+				"type":"plain_text",
+				"text": "Will you choose " + text + "?"
+			},
+			"accessory": {
+				"type": "button",
+				"text": {
+					"type": "plain_text",
+					"text": text 
+				},
+				"action_id": "play_click",
+				"value": text
+			}
+		};
+	return returnVal;
+}
 // Initializes your app with your bot token and signing secret
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
@@ -30,23 +49,7 @@ app.message('play', ({message, say}) => {
 		]
 	};
 	for(var i = 0; i < options.length; i++) {
-		response.blocks.push( 
-			{
-			"type":"section",
-			"text": {
-				"type":"plain_text",
-				"text": "Will you choose " + options[i] + "?"
-			},
-			"accessory": {
-				"type": "button",
-				"text": {
-					"type": "plain_text",
-					"text": options[i]
-				},
-				"action_id": "play_click",
-				"value": options[i]
-			}
-		});
+		response.blocks.push(MakeButtonCard(options[i]));
 	}
 
 	say(response);
@@ -54,6 +57,30 @@ app.message('play', ({message, say}) => {
 		console.log("throwin up");
 		say(`${options[lastPick]}`);
 	}
+});
+
+app.message('put up hands', ({message, say}) => {
+	say(`Pick a card from the first hand`);
+	response = {blocks: []};
+	for(var i = 0; i < handOne.length; i++) {
+		response.blocks.push({
+			"type":"section",
+			"text": {
+				"type":"plain_text",
+				"text": "Will you choose " + handOne[i] + "?"
+			},
+			"accessory": {
+				"type": "button",
+				"text": {
+					"type": "plain_text",
+					"text": handOne[i] 
+				},
+				"action_id": "hands_click_one",
+				"value": handOne[i]
+			}
+		});
+	}
+	say(response);
 });
 		
 app.message('hello', ({message, say}) => {
@@ -100,6 +127,65 @@ app.action('play_click', ({body, ack, say}) => {
 		say(`sorry bub`);
 	}
 	say(`You have ${points} points`);
+});
+
+app.action('hands_click_one', ({body, ack, say}) => {
+	ack();
+	selections.one = body.actions[0].value;
+	say(`Pick a card from the second hand`);
+	response = {blocks: []};
+	for(var i = 0; i < handTwo.length; i++) {
+		response.blocks.push({
+			"type":"section",
+			"text": {
+				"type":"plain_text",
+				"text": "Will you choose " + handTwo[i] + "?"
+			},
+			"accessory": {
+				"type": "button",
+				"text": {
+					"type": "plain_text",
+					"text": handTwo[i] 
+				},
+				"action_id": "hands_click_two",
+				"value": handTwo[i]
+			}
+		});
+	}
+	say(response);
+});
+
+app.action('hands_click_two', ({body, ack, say}) => {
+	ack();
+	selections.two = body.actions[0].value;
+	say(`Okay, now pick a winner`);
+	console.log(selections);
+	let users = Object.keys(selections);
+	response = {blocks: []};
+	for(var i = 0; i < users.length; i++) {
+		response.blocks.push({
+			"type":"section",
+			"text": {
+				"type":"plain_text",
+				"text": "Will you choose " + selections[users[i]] + "?"
+			},
+			"accessory": {
+				"type": "button",
+				"text": {
+					"type": "plain_text",
+					"text": selections[users[i]] 
+				},
+				"action_id": "hands_click_three",
+				"value": selections[users[i]]
+			}
+		});
+	}
+	say(response);
+});
+
+app.action('hands_click_three', ({body, ack, say}) => {
+	ack();
+	say(`You picked ${body.actions[0].value}`);
 });
 
 (async () => {
