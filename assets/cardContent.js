3e62c01cb07d51ae64df445a16160b7606e67066
gamesketches <sam.von.ehren@gmail.com>
2020-03-08 17:07:47 -0400

    Adding in card assets

---
+++ assets/cardContent.js
@@ -0,0 +1,4 @@
+cardContent = {cards:["Your crush gave you a new phone. Now you can share secret text messages and pictures just between you     two.",
+ "The guy you have a big crush is on is having money problems. He needs your help and asks you for a fav    or.",
+ "The person you're dating gies you a bracelet and you have no idea why.",
+ "Porn pops up on your boyfriend's computer."]};
